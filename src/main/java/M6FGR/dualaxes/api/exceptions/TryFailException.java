259c2ca3986a4323427daadb0448ad51a8eb96ca
M6FGR <azooz77899@gmail.com>
2026-03-19 19:45:23 +0300

    updated to 21.1.2

--- src/main/java/M6FGR/dualaxes/api/exceptions/TryFailException.java
+++
@@ -1,7 +0,0 @@
-package M6FGR.dualaxes.api.exceptions;
-
-public class TryFailException extends RuntimeException {
-    public TryFailException(String message) {
-        super(message);
-    }
-}
--- src/main/java/M6FGR/dualaxes/skill/DualAxePassiveSkill.java
+++
@@ -1,9 +0,0 @@
-package M6FGR.dualaxes.skill;
-import yesman.epicfight.skill.SkillBuilder;
-import yesman.epicfight.skill.passive.PassiveSkill;
-public class DualAxePassiveSkill extends PassiveSkill {
-
-    public DualAxePassiveSkill(SkillBuilder<?> builder) {
-        super(builder);
-    }
-}
